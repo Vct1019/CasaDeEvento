c78d5636a56ee19c9319f3a50d611c841a3564cc
Vct1019 <valentina.correa@mariaelvinia.edu.co>
2020-09-29 16:15:41 -0500

    Version 1.0
    creacion de instancias de objetos

--- src/casadeevento/CasaDeEvento.java
+++ src/casadeevento/CasaDeEvento.java
@@ -1,21 +1,105 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package casadeevento;
+package CasaDeEvento;
 
-/**
- *
- * @author usuario
- */
+import  java.util.Date;
+
+
 public class CasaDeEvento {
 
+    private String nombre;
+    private int numeroDeTrabajadores;
+    private Date fechaDeFundacion;
+    private String especialidad;
+    private Boolean nacional;
+
+    public CasaDeEvento(
+            String nombre, 
+            int numeroDeTrabajadores,
+            Date fechaDeFundacion, 
+            String especialidad,
+            Boolean nacional) {
+        this.nombre = nombre;
+        this.numeroDeTrabajadores = numeroDeTrabajadores;
+        this.fechaDeFundacion = fechaDeFundacion;
+        this.especialidad = especialidad;
+        this.nacional = nacional;
+    }
+    
+    //El metodo aumenta la cantidad de ayudantes y decoradoresen 50
+    //y sigue siendo nacional 
+    public void contratarDecoradores(){
+        this.numeroDeTrabajadores += 50;
+        this.nacional = true;
+    }
+    
     /**
-     * @param args the command line arguments
-     */
+    * Este metodo permite darle nombres diferentes a cada evento 
+    */
+    public void nombresUnicos(
+            String nuevoNombre, 
+            String nuevaEspecialidad){
+        this.nombre = nuevoNombre;
+        this.especialidad = nuevaEspecialidad; 
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNumeroDeTrabajadores() {
+        return numeroDeTrabajadores;
+    }
+
+    public void setNumeroDeTrabajadores(int numeroDeTrabajadores) {
+        this.numeroDeTrabajadores = numeroDeTrabajadores;
+    }
+
+    public Date getFechaDeFundacion() {
+        return fechaDeFundacion;
+    }
+
+    public void setFechaDeFundacion(Date fechaDeFundacion) {
+        this.fechaDeFundacion = fechaDeFundacion;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public Boolean getNacional() {
+        return nacional;
+    }
+
+    public void setNacional(Boolean nacional) {
+        this.nacional = nacional;
+    }
+   
+   
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+      CasaDeEvento CasaDeEventouno = new CasaDeEvento(
+              "Abrakadabra",
+              100,
+              new Date(),
+              "Organizadoras de eventos",
+              false);
+      
+        System.out.println(CasaDeEventouno.getNombre());
+        System.out.println(CasaDeEventouno.getNumeroDeTrabajadores());
+        System.out.println(CasaDeEventouno.getNacional());
+        
+        CasaDeEventouno.contratarDecoradores();
+        
+        System.out.println(CasaDeEventouno.getNombre());
+        System.out.println(CasaDeEventouno.getNumeroDeTrabajadores());
+        System.out.println(CasaDeEventouno.getNacional());
     }
     
 }
